48ef2c5476d2a16263f992b7c174e888c810da52
Gjermund Brovold <gjermund.brovold@gmail.com>
2025-03-27 16:06:20 +0100

    autoformat charger

--- charger/distance_sensor.py
+++ charger/distance_sensor.py
@@ -1,5 +1,6 @@
 import RPi.GPIO as GPIO
 import time
+
 GPIO.setmode(GPIO.BCM)
 
 TRIG = 23
@@ -11,15 +12,12 @@ GPIO.setup(TRIG, GPIO.OUT)
 GPIO.setup(ECHO, GPIO.IN)
 
 
-
 my_distance = 200000
 
 while my_distance > 4:
-    
     GPIO.output(TRIG, False)
     print("Waiting for sensor to settle")
     time.sleep(2)
-
 
     GPIO.output(TRIG, True)
     time.sleep(0.00001)
@@ -30,20 +28,17 @@ while my_distance > 4:
 
     while GPIO.input(ECHO) == 1:
         pulse_end = time.time()
-        
+
     pulse_duration = pulse_end - pulse_start
 
     distance = pulse_duration * 17150
 
-    distance= round(distance, 2)
-    
+    distance = round(distance, 2)
+
     my_distance = distance
 
     print("Distance:", distance, "cm")
-    
+
 print("Distance:", distance, "cm")
 
 GPIO.cleanup()
-    
-
-
