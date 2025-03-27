0f9cb1df6e7ab7cb7773ccc420ffd3d84cbd4f02
Gjermund Brovold <gjermund.brovold@gmail.com>
2025-03-27 17:07:48 +0100

    refacotr MQTT_topics

--- scooter/MQTT_TOPICS.py
+++ scooter/MQTT_TOPICS.py
@@ -1,11 +1,1 @@
-# MQTT server config
-MQTT_BROKER = 'mqtt20.iik.ntnu.no' 
-MQTT_PORT = 1883 
-
-# Generel MQTT topics
-MQTT_TOPIC_SCOOTER = '10/scooter' 
-MQTT_TOPIC_CHARGER = '10/charger'
-
-# Scooter drive MQTT topics
-MQTT_TOPIC_SCOOTER_DRIVE = 10/scooter/drive_status
-
+MQTT_TOPICS.py
