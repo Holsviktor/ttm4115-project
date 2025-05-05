f407cc044ed9387e9ad54bab3c7a5fb15eabbd5f
sikvesta <sikvesta@hotmail.com>
2025-05-05 20:20:22 +0200

    added scooter with fixed position for testing

--- state_machines/scooter_stm_spawner.py
+++ state_machines/scooter_stm_spawner.py
@@ -270,7 +270,8 @@ class ScooterManager:
             self._logger.debug(f'Initializing Scooter STM with name "scooter{i}"') 
             self.scooters.append(f'scooter{i}')
             random.seed = random.randint(0, 1337)
-            ScooterLogic(f'scooter{i}', self, x = random.randint(0, 988), y = random.randint(0, 661))
+            ScooterLogic(f'scooter{i}', self, x = random.randint(0, 988), y = random.randint(0, 661)) 
+        ScooterLogic(f'test_discount', self, x = 510, y = 360)
         
         
     def stop(self): 
